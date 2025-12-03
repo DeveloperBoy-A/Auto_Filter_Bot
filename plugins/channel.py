cdb0b650c940a7174af06619cef436cd2d41fc9f
𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫_𝐁𝐨𝐲 <smartmeenaankit786@gmail.com>
2025-12-04 02:06:31 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -307,8 +307,6 @@ async def _process_with_lock(bot, filename, caption, media_info, base_name, proc
         movie_doc["files"].append(file_data)
         schedule_update(bot, base_name)
 
-from pyrogram.types import InputMediaPhoto
-
 async def send_movie_update(bot, base_name):
     max_retries = 3
     base_delay = 5
@@ -327,22 +325,16 @@ async def send_movie_update(bot, base_name):
             [
                 InlineKeyboardButton('♻️ Hᴏᴡ Tᴏ Dᴏᴡɴʟᴏᴀᴅ ♻️', url="https://t.me/newmovies_support/653"
                 )],
-            [
+                                            [
                 InlineKeyboardButton('🔰MOVIE SEARCH GROUP🔰', url="https://t.me/newmovieswebseries_group"
                 )
             ]])
 
             if movie_doc.get("poster_url") and not LINK_PREVIEW:
-                resized_poster = await fetch_image(
-                    movie_doc["poster_url"],
-                    size=(2560, 1440) if LANDSCAPE_POSTER and TMDB_POSTER and not error_tmdb else (853, 1280)
-                )
-                
-                # Spoiler-compatible send
-                media = InputMediaPhoto(media=resized_poster, spoiler=True)
+                resized_poster = await fetch_image(movie_doc["poster_url"], size=(2560, 1440) if LANDSCAPE_POSTER and TMDB_POSTER and not error_tmdb else (853, 1280))
                 msg = await bot.send_photo(
                     chat_id=MOVIE_UPDATE_CHANNEL,
-                    photo=media,
+                    photo=resized_poster,
                     caption=text,
                     reply_markup=buttons,
                     parse_mode=enums.ParseMode.HTML
