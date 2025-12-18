8aa6350acbc2aa424c70a2b08bb561dab0c5f2e4
𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫_𝐁𝐨𝐲 <smartmeenaankit786@gmail.com>
2025-12-18 12:27:15 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1786,29 +1786,32 @@ async def auto_filter(client, msg, spoll=False):
     if offset != "":
         req = message.from_user.id if message.from_user else 0
         try:
-            if settings['max_btn']:
-                btn.append(
-                    [InlineKeyboardButton("ᴘᴀɢᴇ", callback_data="pages"), InlineKeyboardButton(
-                        text=f"1/{math.ceil(int(total_results)/10)}", callback_data="pages"), InlineKeyboardButton(text="ɴᴇxᴛ ⋟", callback_data=f"next_{req}_{key}_{offset}")]
+    if settings.get('max_btn'):
+        btn.append(
+            [
+                InlineKeyboardButton("ᴘᴀɢᴇ", callback_data="pages"),
+                InlineKeyboardButton(
+                    text=f"1/{math.ceil(int(total_results)/10)}", callback_data="pages"
+                ),
+                InlineKeyboardButton(
+                    text="ɴᴇxᴛ ⋟", callback_data=f"next_{req}_{key}_{offset}"
                 )
-            else:
-                btn.append(
-    [
-        InlineKeyboardButton("ᴘᴀɢᴇ", callback_data="pages"),
-        InlineKeyboardButton(
-            text=f"1/{math.ceil(int(total_results)/int(MAX_B_TN))}",
-            callback_data="pages"
-        ),
-        InlineKeyboardButton(
-            text="ɴᴇxᴛ ⋟",
-            callback_data=f"next_{req}_{key}_{offset}"
+            ]
         )
-    ]
-)
-
+    else:
+        btn.append(
+            [
+                InlineKeyboardButton("ᴘᴀɢᴇ", callback_data="pages"),
+                InlineKeyboardButton(
+                    text=f"1/{math.ceil(int(total_results)/int(MAX_B_TN))}", callback_data="pages"
+                ),
+                InlineKeyboardButton(
+                    text="ɴᴇxᴛ ⋟", callback_data=f"next_{req}_{key}_{offset}"
+                )
+            ]
+        )
 except KeyError:
     await save_group_settings(message.chat.id, 'max_btn', True)
-
     btn.append(
         [
             InlineKeyboardButton(
@@ -1818,12 +1821,12 @@ except KeyError:
         ]
     )
 
-    imdb = None
-    if settings.get("imdb") and files and len(files) < 20:
-        imdb = await get_poster(
-            search,
-            file=files[0].file_name
-        )
+imdb = None
+if settings.get("imdb") and files and len(files) < 20:
+    imdb = await get_poster(
+        search,
+        file=files[0].file_name
+    )
 
     cur_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
     time_difference = timedelta(hours=cur_time.hour, minutes=cur_time.minute, seconds=(cur_time.second+(cur_time.microsecond/1000000))) - \
