c93d7356a5baf81752c6e58fd6e7f6a25842ad1b
𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫_𝐁𝐨𝐲 <smartmeenaankit786@gmail.com>
2025-12-18 12:57:39 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1785,8 +1785,8 @@ async def auto_filter(client, msg, spoll=False):
 
     if offset != "":
         req = message.from_user.id if message.from_user else 0
-        try:
-        if settings.get('max_btn'):
+try:
+    if settings.get('max_btn'):
         btn.append(
             [
                 InlineKeyboardButton("ᴘᴀɢᴇ", callback_data="pages"),
