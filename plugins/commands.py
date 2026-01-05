cb9c3359fe394e2bb26a8b837536da8ebf12f4fa
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-01-05 05:46:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -372,10 +372,7 @@ async def start(client, message):
                     reply_markup=InlineKeyboardMarkup(btn)
                 )
                 filesarr.append(msg)
-            k = await client.send_message(chat_id=message.from_user.id, text=DEL_MSG.format(message.from_user.mention, get_time(DELETE_TIME)),  # Hardcoded text की जगह DEL_MSG
-                parse_mode="html",
-                reply_markup=InlineKeyboardMarkup(btn) if btn else None
-            )
+            k = await client.send_message(chat_id=message.from_user.id, text=f"<b><u>❗️⚠️ IMPORTANT WARNING ⚠️❗️</u></b>\n\nᴛʜɪꜱ ᴍᴏᴠɪᴇ ꜰɪʟᴇ/ᴠɪᴅᴇᴏ ᴡɪʟʟ ʙᴇ AUTOMATICALLY DELETED ɪɴ <b><u><code>{get_time(DELETE_TIME)}</code></u> 🫥 <i></b>(ᴅᴜᴇ ᴛᴏ ᴄᴏᴘʏʀɪɢʜᴛ ɪꜱꜱᴜᴇꜱ)</i>.\n\n<b><i>ᴘʟᴇᴀꜱᴇ ꜰᴏʀᴡᴀʀᴅ ᴛʜɪꜱ ꜰɪʟᴇ ᴛᴏ ꜱᴏᴍᴇᴡʜᴇʀᴇ ᴇʟꜱᴇ ᴀɴᴅ ꜱᴛᴀʀᴛ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴛʜᴇʀᴇ.\n\n⚠️ यह फ़ाइल थोड़ी देर में स्वचालित रूप से हटा दी जाएगी, इसलिए इसे अपने saved massage पर अग्रेषित करें और फिर डाउनलोड करें।</i></b>")
             await asyncio.sleep(DELETE_TIME)
             for x in filesarr:
                 await x.delete()
@@ -433,7 +430,11 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(btn)
             )
             k = await msg.reply(
-                DEL_MSG.format(message.from_user.mention, get_time(DELETE_TIME)),
+                f"<b><u>❗️⚠️ IMPORTANT WARNING ⚠️❗️</u></b>\n\n"
+f"THIS MOVIE FILE/VIDEO WILL BE <b>AUTOMATICALLY DELETED</b> IN <b><u><code>{get_time(DELETE_TIME)}</code></u></b> 🫥 "
+"(DUE TO COPYRIGHT ISSUES).\n\n"
+f"<b><i>PLEASE FORWARD THIS FILE TO SOMEWHERE ELSE AND START DOWNLOADING THERE.\n\n"
+f"⚠️ यह फ़ाइल  <u><code>{get_time(DELETE_TIME)}</code></u> में स्वचालित रूप से हटा दी जाएगी, इसलिए इसे अपने  SAVED MESSAGES पर अग्रेषित करें और फिर डाउनलोड करें।</i></b>",
                 quote=True
             )
             await asyncio.sleep(DELETE_TIME)
@@ -487,7 +488,11 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(btn)
     )
     k = await msg.reply(
-        DEL_MSG.format(message.from_user.mention, get_time(DELETE_TIME)),
+        f"<b><u>❗️⚠️ IMPORTANT WARNING ⚠️❗️</u></b>\n\n"
+f"THIS MOVIE FILE/VIDEO WILL BE <b>AUTOMATICALLY DELETED</b> IN <b><u><code>{get_time(DELETE_TIME)}</code></u></b> 🫥 "
+"(DUE TO COPYRIGHT ISSUES).\n\n"
+f"<b><i>PLEASE FORWARD THIS FILE TO SOMEWHERE ELSE AND START DOWNLOADING THERE.\n\n"
+f"⚠️ यह फ़ाइल  <u><code>{get_time(DELETE_TIME)}</code></u> में स्वचालित रूप से हटा दी जाएगी, इसलिए इसे अपने  SAVED MESSAGES पर अग्रेषित करें और फिर डाउनलोड करें।</i></b>",
         quote=True
     )     
     await asyncio.sleep(DELETE_TIME)
