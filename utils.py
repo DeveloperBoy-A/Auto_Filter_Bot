fc3b617847431e3f7e9af8ea08b4bd51ea7d9a16
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-02-06 23:46:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -625,7 +625,7 @@ async def save_group_settings(group_id, key, value):
     await db.update_settings(group_id, current)
 
 def clean_filename(file_name):
-    prefixes = ('[', '@', 'www.', 'ClipmateZone', 'NewMoviesOnTG', 'moviehub4uupdate', 'moviehub4u', 'update', 'New', 'Movies', 'OnTG', 'HD', 'FILMSCLUB04', )
+    prefixes = ('[', '@', 'www.', 'ClipmateZone', 'NewMoviesOnTG', 'moviehub4uupdate', 'moviehub4u', 'update', 'New', 'Movies', 'OnTG', 'FILMSCLUB04', )
     unwanted = {word.lower() for word in BAD_WORDS}
 
     file_name = ' '.join(
