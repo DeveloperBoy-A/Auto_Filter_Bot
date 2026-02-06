6f38e1dc1edcc61d4430845466f76e280ac5ddb8
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-02-06 23:19:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1081,7 +1081,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                             f"<b>{idx}. "
                             f"<a href='https://telegram.me/{temp.U_NAME}"
                             f"?start=file_{query.message.chat.id}_{file.file_id}'>"
-                            f"[{get_size(file.file_size)}]"
+                            f"[{get_size(file.file_size)}] "
                             f"{clean_filename(file.file_name)}\n\n"
                             f"</a></b>"
                         )
@@ -1127,7 +1127,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                             f"<b>{idx}. "
                             f"<a href='https://telegram.me/{temp.U_NAME}"
                             f"?start=file_{query.message.chat.id}_{file.file_id}'>"
-                            f"[{get_size(file.file_size)}]"
+                            f"[{get_size(file.file_size)}] "
                             f"{clean_filename(file.file_name)}\n\n"
                             f"</a></b>"
                         )
@@ -1147,7 +1147,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                             f"<b>{idx}. "
                             f"<a href='https://telegram.me/{temp.U_NAME}"
                             f"?start=file_{query.message.chat.id}_{file.file_id}'>"
-                            f"[{get_size(file.file_size)}]"
+                            f"[{get_size(file.file_size)}] "
                             f"{clean_filename(file.file_name)}\n\n"
                             f"</a></b>"
                         )
@@ -1166,7 +1166,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                             f"<b>{idx}. "
                             f"<a href='https://telegram.me/{temp.U_NAME}"
                             f"?start=file_{query.message.chat.id}_{file.file_id}'>"
-                            f"[{get_size(file.file_size)}]"
+                            f"[{get_size(file.file_size)}] "
                             f"{clean_filename(file.file_name)}\n\n"
                             f"</a></b>"
                         )
