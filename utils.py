49e99f7d3ae0346daccb72610bb4a66a54d52238
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-03-24 20:48:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -27,7 +27,8 @@ BTN_URL_REGEX = re.compile(
 )
 
 
-imdb = IMDBKit()  
+imdb = IMDBKit()
+imdb = Cinemagoer()   
 BANNED = {}
 SMART_OPEN = '“'
 SMART_CLOSE = '”'
@@ -339,7 +340,7 @@ BLOCKED_WORDS = [
     'interview'
 ]
 
-async def old_get_poster(query, bulk=False, id=False, file=None):
+async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
         query = (query.strip()).lower()
         title = query
