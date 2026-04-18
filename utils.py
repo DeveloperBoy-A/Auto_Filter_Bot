aefbfc3ac322339ccd262071bb60dee51e39107e
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-04-18 15:28:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,8 @@ from info import  *
 from imdb import Cinemagoer 
 from imdbkit import IMDBKit
 from rapidfuzz import fuzz
-from fuzzywuzzy import process 
+from rapidfuzz import process
+#from fuzzywuzzy import process 
 #from urllib.parse import quote_plus
 import asyncio
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
