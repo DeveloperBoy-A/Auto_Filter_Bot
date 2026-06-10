c73ee11ecfef8f1437a760e8ee6372bf231aef95
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-06-10 19:40:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ FSUB_PICS = (environ.get('FSUB_PICS', 'https://i.ibb.co/cXrcfZPN/photo-2026-04-2
 # Admin, Channels & Users Configuration
 # ============================
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5308609720').split()] # Replace with the actual admin ID(s) to add
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002325405552 -1002389198193').split()]  # Channel id for auto indexing (make sure bot is admin)
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002325405552 -1002389198193 -1003951763196').split()]  # Channel id for auto indexing (make sure bot is admin)
 
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002477743748'))  # Log channel id (make sure bot is admin)
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002269396456'))  # Bin channel id (make sure bot is admin)
