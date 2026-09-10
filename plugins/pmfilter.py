30e8ec60f71c8e8c523335edc22aa4e458787e22
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-09-10 20:39:43 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2410,15 +2410,19 @@ async def ai_spell_check(chat_id, wrong_name):
     movie_list = await search_movie(wrong_name)
     if not movie_list:
         return
-    for _ in range(5):
+    for _ in range(4):
         closest_match = process.extractOne(wrong_name, movie_list)
-        if not closest_match or closest_match[1] <= 70:
+        if not closest_match or closest_match[1] <= 65:
             return
         movie = closest_match[0]
+        movie = re.sub(r"[-:.,&]", " ", movie)
+        movie = re.sub(r"[!@#$%^*()_+=\[\]{};\"<>?/\\|]", " ", movie)
+        movie = re.sub(r"\s+", " ", movie).strip()
         files, _, _ = await get_search_results(chat_id=chat_id, query=movie)
         if files:
             return movie
         movie_list.remove(movie)
+
 
 async def old_advantage_spell_chok(client, message):
     search = message.text
