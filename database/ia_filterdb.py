6f7354f0382011ce77cf96545a0d8f500ea0f8f9
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-09-13 18:20:26 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -586,7 +586,7 @@ def extract_pure_title(original_name):
         r'(?:19|20)\d{2}',                                  
         r'combined', r'complete',                           
         r'dual[\s\-]?audio', r'multi[\s\-]?audio',          
-        r'hdtc', r'@', r'tamil', r'telugu', r'malayalam', r'kannada', r'bengali', r'marathi', r'korean', r'japanese', r'chinese', r'spanish', r'russian', r'french',
+        r'hdtc', r'@', r'tamil', r'telugu', r'malayalam', r'kannada', r'bengali', r'marathi', r'korean', r'japanese', r'chinese', r'spanish', r'russian', r'streamershub', r'french',
         r'web[\-\s]?dl', r'web[\-\s]?rip', r'hdrip', r'bluray', r'brrip', r'dvdrip', r'camrip', r'hdts', r'hdcam', 
         r'av1', r'x264', r'x265', r'hevc', r'10bit', r'aac', r'eac3', r'ac3', r'ddp[\s\-]?7\.1', r'ddp[\s\-]?5\.1', r'dd[\s\-]?5\.1', r'dd[\s\-]?2\.0', r'ddp', r'5\.1', r'7\.1', r'2\.0', r'2ch', r'stereo',
          r'full[\s\-]?movie', r'web[\s\-]?series', 
