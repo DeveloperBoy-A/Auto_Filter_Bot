d7e43b18ccc0713595381f240b1011497672b9d7
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-09-17 14:36:59 +0530

    Update refer.py

--- database/refer.py
+++ database/refer.py
@@ -1,11 +1,11 @@
-import pymongo
+from motor.motor_asyncio import AsyncIOMotorClient
 from info import DATABASE_URI, DATABASE_NAME
 import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(DATABASE_URI)
+myclient = AsyncIOMotorClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
 
@@ -14,25 +14,25 @@ class UserTracker:
         self.user_collection = mydb["referusers"]
         self.refer_collection = mydb["refers"]
 
-    def add_user(self, user_id):
-        if not self.is_user_in_list(user_id):
-            self.user_collection.insert_one({'user_id': user_id})
+    async def add_user(self, user_id):
+        if not await self.is_user_in_list(user_id):
+            await self.user_collection.insert_one({'user_id': user_id})
 
-    def remove_user(self, user_id):
-        self.user_collection.delete_one({'user_id': user_id})
+    async def remove_user(self, user_id):
+        await self.user_collection.delete_one({'user_id': user_id})
 
-    def is_user_in_list(self, user_id):
-        return bool(self.user_collection.find_one({'user_id': user_id}))
+    async def is_user_in_list(self, user_id):
+        return bool(await self.user_collection.find_one({'user_id': user_id}))
 
-    def add_refer_points(self, user_id: int, points: int):
-        self.refer_collection.update_one(
+    async def add_refer_points(self, user_id: int, points: int):
+        await self.refer_collection.update_one(
             {'user_id': user_id},
             {'$set': {'points': points}},
             upsert=True
         )
 
-    def get_refer_points(self, user_id: int):
-        user = self.refer_collection.find_one({'user_id': user_id})
+    async def get_refer_points(self, user_id: int):
+        user = await self.refer_collection.find_one({'user_id': user_id})
         return user.get('points') if user else 0
 
 
