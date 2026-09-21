d43c9ec8d9c7362f54b54778983c15e08b4c6f8a
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-09-21 17:12:35 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -481,11 +481,19 @@ class Database:
             return False
 
     async def get_expired(self, current_time):
-        expired_users = []
-        if data := self.users.find({"expiry_time": {"$lt": current_time}}):
-            async for user in data:
-                expired_users.append(user)
-        return expired_users
+    expired_users = []
+
+    data = self.users.find({
+        "expiry_time": {
+            "$ne": None,
+            "$lt": current_time
+        }
+    })
+
+    async for user in data:
+        expired_users.append(user)
+
+    return expired_users
 
     async def remove_premium_access(self, user_id):
         return await self.update_one(
