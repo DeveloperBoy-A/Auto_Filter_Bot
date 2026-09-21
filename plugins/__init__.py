5301ba13f42c89107731da47ebd4c03f81c51c02
『𓆩ꨄ︎𓆪Mʀ⛦ᴀɴᴋɪᴛ ᴍᴇᴇणा𓆩ꨄ︎𓆪』 <smartmeenaankit786@gmail.com>
2026-09-21 17:32:29 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,7 +5,7 @@ from asyncio import sleep
 from datetime import datetime
 from database.users_chats_db import db
 from info import LOG_CHANNEL, URL, PREMIUM_LOGS
-from Script import script
+PREMIUM_END_TEXT = script.PREMIUM_END_TEXT
 import aiohttp
 import asyncio
 import logging
